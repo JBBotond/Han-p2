07b4f0a0cec49659d3420783094f331b7f55d8c1
Pluto <149091525+bichevlover123@users.noreply.github.com>
2025-12-17 14:03:19 +0100

    events final

--- prog2/tea-machine-code/events.h
+++ prog2/tea-machine-code/events.h
@@ -7,11 +7,13 @@ typedef enum {
    E_CONTINUE,          ///< Initialising subsystems is ready
    E_CONTINUE_ERROR,    ///< Initialisation error
    E_CONFIG_READY,      ///< User action is open gate
-   E_20C,               ///< Coin acceptor detects 20c coin
-   E_50C,               ///< Coin acceptor detects 50c coin
    E_NOT_ENOUGH,        ///< Total inserted money is not enough
    E_ENOUGH,            ///< Total inserted money is enough
    E_EXIT_SYSTEM,       ///< System exception or shutdown
+   E_SELECT_DRINK,      ///< Select a desired drink
+   E_POUR,              ///< Pouring the desired drink
+   E_WAIT,              ///< Waiting for infusion
+   E_STILL_MAKING,      ///< Still being made
 } event_t;
 
 #endif
