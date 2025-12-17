87a06866194f23f66b2ebcc631ebc61bd2de1cdd
JBBotond <janosibboti@gmail.com>
2025-12-17 14:00:28 +0100

    states added

--- prog2/tea-machine-code/states.c
+++ prog2/tea-machine-code/states.c
@@ -3,9 +3,11 @@ char * stateEnumToText[] =
    "S_NO",                         ///< Used for initialisation if state is not yet known
    "S_START",                      ///< Initial state
    "S_INITIALISESUBSYSTEMS",       ///< Initialised subsystems
-   "S_CONFIGURE",                  ///< Ask user for action
-   "S_PROCESS_20C",                ///< Handle the 20c insert
-   "S_PROCESS_50C",                ///< Handle the 50c insert
-   "S_WAITFORCOINS",               ///< Wait for the user to insert coins
+   "S_CONFIGURE",                  ///< Ask user for action               ///< Handle the 50c insert
+   "S_WAITFORPAYMENT",               ///< Wait for the user to insert coins
    "S_SHUTDOWN_SYSTEM",            ///< Exception or system exit shutdown requested
+    "S_IDLE",		/// idle, wait for user selection
+    "S_INTERFACE",	/// ask user for money --> enough/not enough
+    "S_MAKEDRINK",	/// drink is in the making
+    "S_DONEDRINK"	/// drink is given to user
 };
